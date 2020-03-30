43a2717bda41dd2b6b0b39c3c88cf8465dd2af98
FifteenWheat <55380885+FifteenWheat@users.noreply.github.com>
2020-03-30 12:00:29 +0200

    Add files via upload

---
+++ FootGolf/FootGolf/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace footgolf
+{
+    class Program
+    {
+        static List<versenyzo> versenyzok;
+        static void Main(string[] args)
+        {
+            feltolt();
+            f3();
+            f4();
+            f6();
+            f7();
+            f8();
+            Console.ReadKey();
+        }
+
+        private static void f8()
+        {
+            foreach (var t in versenyzok.GroupBy(v => v.egyesulet).Select(group => new {karegoria = group.Key, szam = group.Count()}))
+            {
+                if (t.karegoria != "n.a." && t.szam>2)
+                {
+                    Console.WriteLine(t.karegoria + " " + t.szam);
+                }
+            }
+        }
+
+        private static void f7()
+        {
+            var sw = new StreamWriter(@"osszpontFF.txt",true);
+            foreach (var v in versenyzok)
+            {
+                if (v.kategoria == "Felnott ferfi") sw.WriteLine(v.nev + ";" + v.oszpontszam());
+            }
+            sw.Close();
+        }
+
+        private static void f6()
+        {
+            Console.WriteLine("6. feladat: A bajnok női versenyző");
+            versenyzo v = versenyzok.Where(f => f.kategoria == "Noi").OrderBy(f => f.oszpontszam()).Last();
+            Console.WriteLine("\tNév: "+v.nev+"\n\tEgyesület: "+v.egyesulet+"\n\tÖsszpont: "+v.oszpontszam());
+        }
+
+        private static void f4()
+        {
+            double db = versenyzok.Count;
+            double eredmeny = versenyzok.Count(f => f.kategoria == "Noi") / db;
+            Console.WriteLine("4. feladat: A női versenyzők aránya: "+Math.Round((double)(eredmeny*100), 2));
+        }
+
+        private static void f3()
+        {
+            Console.WriteLine("3. feladat: Versenyzők száma: "+versenyzok.Count);
+        }
+
+        private static void feltolt()
+        {
+            versenyzok = new List<versenyzo>();
+            var sr = new StreamReader(@"..\..\Resources\fob2016.txt", Encoding.UTF8);
+            while (!sr.EndOfStream) versenyzok.Add(new versenyzo(sr.ReadLine()));
+            sr.Close();   
+        }
+    }
+}
---
+++ FootGolf/FootGolf/versenyzo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace footgolf
+{
+    class versenyzo
+    {
+        public string nev{ get; set; }
+        public string kategoria { get; set; }
+        public string egyesulet{ get; set; }
+        public int[]pontok { get; set; }
+        public versenyzo(string s)
+        {
+            var t = s.Split(';');
+            this.nev = t[0];
+            this.kategoria = t[1];
+            this.egyesulet = t[2];
+            this.pontok = new int[8];
+            for (int i = 0; i < 8; i++)
+            {
+                this.pontok[i] = int.Parse(t[i+3]);
+            }
+        }
+        public int oszpontszam()
+        {
+            int ossz = 0;
+            Array.Sort(pontok);
+            if (pontok[0] != 0) ossz += 10;
+            if (pontok[1] != 0) ossz += 10;
+            for (int i = 2; i < pontok.Length; i++) ossz += pontok[i];
+            return ossz;
+        }
+    }
+}
